2a29adfd53a631ddb8295b00abd2b82741327031
Kristen Picard <kristenpicard93@gmail.com>
2021-09-28 17:04:02 -0500

    Got Discover page working with info and button to change cocktail

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,35 @@
 import React from "react";
 import "./style.css";
+import CardBtn from "../CardBtn";
 
 function Card(props) {
   return (
+    <>
     <div
       className="card"
       style={{
-        backgroundImage: props.image ? `url(${props.image})` : "none"
+        backgroundImage: props.image
+          ? `url(${props.image})` : "none"
       }}
+      
     >
-      {props.image}
+      {/* {props.image} */}
+      <CardBtn
+        onClick={props.handleBtnClick}
+        data-value="pass"
+      />
       
     </div>
+    <div>
+      Drink Name: {props.name}
+      <br></br>
+      Ingredients: 
+      {props.ingredients}
+      <br></br>
+      Instructions: 
+      {props.instructions}
+    </div>
+    </>
   );
 }
 
--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -5,8 +5,11 @@ import Card from "../components/Card";
 class Discover extends Component {
   state = {
     image: "",
-    match: false,
-    matchCount: 0
+    name: "",
+    ingredients: "",
+    instructions: ""
+    // match: false,
+    // matchCount: 0
   };
 
   // When the component mounts, load the next dog to be displayed
@@ -15,26 +18,26 @@ class Discover extends Component {
   }
 
   handleBtnClick = event => {
-    // Get the data-value of the clicked button
-    const btnType = event.target.attributes.getNamedItem("data-value").value;
-    // Clone this.state to the newState object
-    // We'll modify this object and use it to set our component's state
-    const newState = { ...this.state };
+    // // Get the data-value of the clicked button
+    // const btnType = event.target.attributes.getNamedItem("data-value").value;
+    // // Clone this.state to the newState object
+    // // We'll modify this object and use it to set our component's state
+    // const newState = { ...this.state };
 
-    if (btnType === "pick") {
-      // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
-      newState.match = 1 === Math.floor(Math.random() * 5) + 1;
+    // if (btnType === "pick") {
+    //   // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
+    //   newState.match = 1 === Math.floor(Math.random() * 5) + 1;
 
-      // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
-      newState.matchCount = newState.match
-        ? newState.matchCount + 1
-        : newState.matchCount;
-    } else {
-      // If we thumbs down'ed the dog, we haven't matched with it
-      newState.match = false;
-    }
-    // Replace our component's state with newState, load the next dog image
-    this.setState(newState);
+    //   // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
+    //   newState.matchCount = newState.match
+    //     ? newState.matchCount + 1
+    //     : newState.matchCount;
+    // } else {
+    //   // If we thumbs down'ed the dog, we haven't matched with it
+    //   newState.match = false;
+    // }
+    // // Replace our component's state with newState, load the next dog image
+    // this.setState(newState);
     this.loadNextDog();
   };
 
@@ -42,7 +45,10 @@ class Discover extends Component {
     API.getRandomDrink()
       .then(res =>
         this.setState({
-          image: res.data.drinks[0].strDrinkThumb
+          image: res.data.drinks[0].strDrinkThumb,
+          name: res.data.drinks[0].strDrink,
+          ingredients: res.data.drinks[0].strIngredient1,
+          instructions: res.data.drinks[0].strInstructions
         })
       )
       .catch(err => console.log(err));
@@ -52,7 +58,7 @@ class Discover extends Component {
     return (
       <div>
         
-        <Card image={this.state.image} handleBtnClick={this.handleBtnClick} />
+        <Card image={this.state.image} name={this.state.name} ingredients={this.state.ingredients} instructions={this.state.instructions} handleBtnClick={this.handleBtnClick} />
         
        
       </div>
